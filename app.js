8fc385efe1c5b8cfb27d52928febb8967932b3d5
sunita baram <sunitabaram489@gmail.com>
2025-05-24 10:08:05 +0545

    Adding currency converter files

---
+++ app.js
@@ -0,0 +1,80 @@
+const BASE_URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies';
+
+const dropdowns = document.querySelectorAll('.drop-down select');
+const btn = document.querySelector('form button');
+const fromCurr = document.querySelector('.from select');
+const toCurr = document.querySelector('.to select');
+const msg = document.querySelector('.msg');
+const icon = document.querySelector('i');
+
+for(let select of dropdowns){
+    for(let currCode in countryList){
+        const newOption = document.createElement('option');
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === 'from' && currCode === 'USD'){
+            newOption.selected = "selected";
+        }
+        else if(select.name === 'to' && currCode === 'INR'){
+            newOption.selected = 'selected';
+        }
+        
+        select.append(newOption);
+    }
+    select.addEventListener('change', (evt) => {
+        updateFlag(evt.target);
+    })
+
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector('img');
+    img.src = newSrc;
+}
+
+
+const updateExchangeRate = async() => {
+    let amount = document.querySelector('.amount input');
+    let amtVal = amount.value;
+    if(amtVal === '' || amtVal < 1){
+        amtVal = 1;
+        amount.value = "1";
+    }
+    // console.log(fromCurr.value, toCurr.value);
+    let fromCurrency = fromCurr.value.toLowerCase();
+    let toCurrency = toCurr.value.toLowerCase();
+
+    const URL = `${BASE_URL}/${fromCurrency}.min.json`;
+    
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurrency][toCurrency];
+
+    let finalAmt = amtVal * rate;
+
+    console.log(finalAmt);
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+}
+
+window.addEventListener('load', () =>{
+   updateExchangeRate();
+});
+
+btn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+icon.addEventListener('click', () => {
+    let temp = fromCurr.value;
+    fromCurr.value = toCurr.value;
+    toCurr.value = temp;
+    updateFlag(fromCurr);
+    updateFlag(toCurr);
+    updateExchangeRate();
+    
+});
+
